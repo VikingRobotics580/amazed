bf58785821a6dd88f4dfc44856cbe8c5ff538aa0
ICGStudios <46583403+ICGStudios@users.noreply.github.com>
2020-01-07 16:55:33 -0800

    fcgdf

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -12,26 +12,26 @@ import frc.robot.commands.DriveCommand;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveSubsystem extends Subsystem {
-    WPI_TalonSRX _rghtFront;
-    WPI_TalonSRX _rghtFollower;
-    WPI_TalonSRX _leftFront;
-    WPI_TalonSRX _leftFollower;
+    WPI_TalonSRX _rFront;
+    WPI_TalonSRX _rBack;
+    WPI_TalonSRX _lFront;
+    WPI_TalonSRX _lBack;
 
     DifferentialDrive _diffDrive;
 
-    Faults _faults_L;
-    Faults _faults_R;
+    Faults _lFaults;
+    Faults _rFaults;
 
     public DriveSubsystem() {
-        _rghtFront = new WPI_TalonSRX(2);
-        _rghtFollower = new WPI_TalonSRX(3);
-        _leftFront = new WPI_TalonSRX(0);
-        _leftFollower = new WPI_TalonSRX(1);
+        _rFront = new WPI_TalonSRX(2);
+        _rBack = new WPI_TalonSRX(3);
+        _lFront = new WPI_TalonSRX(0);
+        _lBack = new WPI_TalonSRX(1);
 
-        _diffDrive = new DifferentialDrive(_leftFront, _rghtFront);
+        _diffDrive = new DifferentialDrive(_lFront, _rFront);
     
-        _faults_L = new Faults();
-        _faults_R = new Faults();
+        _lFaults = new Faults();
+        _rFaults = new Faults();
     }
 
     public void Driver() {
@@ -40,7 +40,7 @@ public class DriveSubsystem extends Subsystem {
         /* get gamepad stick values */
         double forw = -1 * rightJoystick.getRawAxis(1); /* positive is forward */
         double turn = +1 * rightJoystick.getRawAxis(2); /* positive is right */
-        boolean btn1 = rightJoystick.getRawButton(1); /* is button is down, print joystick values */
+        boolean btn1 = rightJoystick.getRawButton(1); /* if button is down, print joystick values */
 
         /* deadband gamepad 10% */
         if (Math.abs(forw) < 0.10) {
@@ -62,21 +62,21 @@ public class DriveSubsystem extends Subsystem {
         /* get sensor values */
         // double leftPos = _leftFront.GetSelectedSensorPosition(0);
         // double rghtPos = _rghtFront.GetSelectedSensorPosition(0);
-        double leftVelUnitsPer100ms = _leftFront.getSelectedSensorVelocity(0);
-        double rghtVelUnitsPer100ms = _rghtFront.getSelectedSensorVelocity(0);
+        double leftVelUnitsPer100ms = _lFront.getSelectedSensorVelocity(0);
+        double rghtVelUnitsPer100ms = _rFront.getSelectedSensorVelocity(0);
 
         work += " L:" + leftVelUnitsPer100ms + " R:" + rghtVelUnitsPer100ms;
 
         /*
         * drive motor at least 25%, Talons will auto-detect if sensor is out of phase
         */
-        _leftFront.getFaults(_faults_L);
-        _rghtFront.getFaults(_faults_R);
+        _lFront.getFaults(_lFaults);
+        _rFront.getFaults(_rFaults);
 
-        if (_faults_L.SensorOutOfPhase) {
+        if (_lFaults.SensorOutOfPhase) {
             work += " L sensor is out of phase";
         }
-        if (_faults_R.SensorOutOfPhase) {
+        if (_rFaults.SensorOutOfPhase) {
             work += " R sensor is out of phase";
         }
 
@@ -88,30 +88,30 @@ public class DriveSubsystem extends Subsystem {
 
     public void initDefaultCommand() {
         /* factory default values */
-        _rghtFront.configFactoryDefault();
-        _rghtFollower.configFactoryDefault();
-        _leftFront.configFactoryDefault();
-        _leftFollower.configFactoryDefault();
+        _rFront.configFactoryDefault();
+        _rBack.configFactoryDefault();
+        _lFront.configFactoryDefault();
+        _lBack.configFactoryDefault();
 
         /* set up followers */
-        _rghtFollower.follow(_rghtFront);
-        _leftFollower.follow(_leftFront);
+        _rBack.follow(_rFront);
+        _lBack.follow(_lFront);
 
         /* [3] flip values so robot moves forward when stick-forward/LEDs-green */
-        _rghtFront.setInverted(true); // !< Update this
-        _leftFront.setInverted(false); // !< Update this
+        _rFront.setInverted(true); // !< Update this
+        _lFront.setInverted(false); // !< Update this
 
         /*
         * set the invert of the followers to match their respective master controllers
         */
-        _rghtFollower.setInverted(InvertType.FollowMaster);
-        _leftFollower.setInverted(InvertType.FollowMaster);
+        _rBack.setInverted(InvertType.FollowMaster);
+        _lBack.setInverted(InvertType.FollowMaster);
 
         /*
         * [4] adjust sensor phase so sensor moves positive when Talon LEDs are green
         */
-        _rghtFront.setSensorPhase(true);
-        _leftFront.setSensorPhase(true);
+        _rFront.setSensorPhase(true);
+        _lFront.setSensorPhase(true);
 
         /*
         * WPI drivetrain classes defaultly assume left and right are opposite. call
